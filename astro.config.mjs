c902afccd3703444fe9783126fe60ec67e7267aa
Victor Garcia Mestre <vgmestre@gmail.com>
2022-09-27 23:02:18 +0200

    tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import image from '@astrojs/image';
 
-import image from '@astrojs/image'
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact(), image()]
-})
+  integrations: [preact(), image(), tailwind()]
+});
