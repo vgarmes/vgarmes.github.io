84fe459ddb026c77a89e949076c2c12c9e6e68bd
Victor Garcia Mestre <vgmestre@gmail.com>
2022-09-27 23:48:56 +0200

    base styles

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+module.exports = {
+	env: {
+		node: true,
+		es2022: true,
+		browser: true
+	},
+	extends: ['eslint:recommended', 'plugin:astro/recommended'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	overrides: [
+		{
+			// Define the configuration for `.astro` file.
+			files: ['*.astro'],
+			// Allows Astro components to be parsed.
+			parser: 'astro-eslint-parser',
+			// Parse the script in `.astro` as TypeScript by adding the following configuration.
+			// It's the setting you need when using TypeScript.
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro']
+			},
+			rules: {
+				// override/add rules settings here, such as:
+				// "astro/no-set-html-directive": "error"
+			}
+		},
+		{
+			files: ['*.ts', '*.tsx'],
+			parser: '@typescript-eslint/parser',
+			extends: ['plugin:@typescript-eslint/recommended'],
+			rules: {
+				'@typescript-eslint/no-unused-vars': [
+					'error',
+					{ argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }
+				],
+				'@typescript-eslint/no-non-null-assertion': 'off'
+			}
+		},
+		{
+			// Define the configuration for `<script>` tag.
+			// Script in `<script>` is assigned a virtual file name with the `.js` extension.
+			files: ['**/*.astro/*.js', '*.astro/*.js'],
+			parser: '@typescript-eslint/parser'
+		}
+	]
+}
