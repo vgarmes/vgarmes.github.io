ccf703fb6cd164a9b167b1d8c405b56a755bd4bf
Victor Garcia Mestre <vgmestre@gmail.com>
2022-09-30 11:41:14 +0200

    delete type module

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {}
+		extend: {
+			animation: {
+				fadein: 'fadein 300ms'
+			},
+			keyframes: {
+				fadein: {
+					from: { opacity: 0 },
+					to: { opacity: 1 }
+				}
+			}
+		}
 	},
 	plugins: []
 }
