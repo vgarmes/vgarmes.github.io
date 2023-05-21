d648592a3bbf4e3fb237133cb1113eaa76449cd2
Victor Garcia Mestre <vgmestre@gmail.com>
2023-05-21 22:07:05 +0200

    toc fixes

--- src/components/blog/TableOfContents.tsx
+++ src/components/blog/TableOfContents.tsx
@@ -1,6 +1,5 @@
-import type { FunctionalComponent } from 'preact'
+import type { FunctionalComponent, JSX } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
-import { NAVIGATION_HEIGHT } from '~/config'
 import type { TocItem } from '~/util/generateToc'
 
 interface Props {
@@ -37,16 +36,22 @@ const TableOfContents: FunctionalComponent<Props> = ({ toc }) => {
 		)
 
 		document
-			.querySelectorAll('article :is(h1,h2,h3)')
+			.querySelectorAll('#article-content :is(h1,h2,h3)')
 			.forEach(h => headingsObserver.observe(h))
 
 		return () => headingsObserver.disconnect()
 	}, [])
 
+	const onLinkClick = (e: JSX.TargetedMouseEvent<HTMLAnchorElement>) =>
+		setCurrentHeading({
+			slug: e.currentTarget.getAttribute('href')!.replace('#', ''),
+			text: e.currentTarget.textContent || ''
+		})
+
 	return (
 		<aside
 			role="navigation"
-			className={`sticky top-0 right-0 hidden flex-1 pt-[${NAVIGATION_HEIGHT}] lg:block`}
+			className={`sticky top-0 right-0 hidden flex-1 pt-16 lg:block`}
 		>
 			{toc.length > 0 && (
 				<h2 className="mb-3 w-full px-4 pl-8 text-sm font-bold uppercase tracking-wide lg:mb-3">
@@ -65,9 +70,10 @@ const TableOfContents: FunctionalComponent<Props> = ({ toc }) => {
 									className={`'block py-2 leading-normal ${
 										currentHeading.slug === tocItem.slug
 											? 'font-bold text-pink-600 dark:text-pink-400'
-											: 'opacity-70 hover:opacity-100'
+											: 'font-semibold opacity-70 hover:opacity-100'
 									}`}
 									href={`#${tocItem.slug}`}
+									onClick={onLinkClick}
 								>
 									{tocItem.text}
 								</a>
