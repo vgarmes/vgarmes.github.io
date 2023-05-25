de8b7e27f45436f29e57534ee109b2b8451f5de1
Victor Garcia Mestre <vgmestre@gmail.com>
2023-05-25 23:13:43 +0200

    showcase

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,14 @@
 export const SITE_TITLE = "Victor's homepage"
 export const SITE_DESCRIPTION = 'My personal site'
 export const NAVIGATION_HEIGHT = '60px'
+export const showcase = [
+	{
+		name: 'Game Lib',
+		slug: 'game-lib',
+		url: '/',
+		thumbnail:
+			'https://res.cloudinary.com/dx73a1lse/image/upload/c_fill,g_north,h_494,w_672/portfolio/game-lib_gs8itj.png',
+		description:
+			'An application that helps keeping track of your videogame collection'
+	}
+]
