9868e542d3c84b0abcb5d99015b2ceb8eb758868
Victor Garcia Mestre <vgmestre@gmail.com>
2023-05-29 22:44:03 +0200

    more projects

--- src/config.ts
+++ src/config.ts
@@ -10,5 +10,23 @@ export const showcase = [
 			'https://res.cloudinary.com/dx73a1lse/image/upload/c_fill,g_north,h_494,w_672/portfolio/game-lib_gs8itj.png',
 		description:
 			'An application that helps keeping track of your videogame collection'
+	},
+	{
+		name: 'My Steam',
+		slug: 'my-steam',
+		url: '/',
+		thumbnail:
+			'https://repository-images.githubusercontent.com/589785833/e282849a-fb31-49b1-86cb-e3be80d05a10',
+		description:
+			'An API of your Steam stats easily deployable to a Cloudflare worker'
+	},
+	{
+		name: 'Markdown Editor',
+		slug: 'markdown-editor',
+		url: '/',
+		thumbnail:
+			'https://res.cloudinary.com/dx73a1lse/image/upload/c_fill,h_494,w_672/portfolio/markdown-editor_pple1j.png',
+		description:
+			'A markdown editor written in React using Vite and packaged with Electron'
 	}
 ]
