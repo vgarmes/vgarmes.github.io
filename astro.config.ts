0fcdb2dc59a5fac0b050cf23a403e3b5d129aaeb
Victor Garcia Mestre <vgmestre@gmail.com>
2023-06-29 00:31:59 +0200

    add read time to posts

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,13 @@ import preact from '@astrojs/preact'
 import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind' // TODO: set 'site' (https://docs.astro.build/en/reference/configuration-reference/#site)
 import mdx from '@astrojs/mdx'
+import { remarkReadingTime } from './remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [preact({ compat: true }), image(), tailwind(), mdx()],
-	site: 'https://vgarmes.github.io'
+	site: 'https://vgarmes.github.io',
+	markdown: {
+		remarkPlugins: [remarkReadingTime]
+	}
 })
