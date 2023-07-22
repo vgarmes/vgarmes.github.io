7bdfc7d4688b3e9e9e28652cb9b2ac672fdf8da2
Victor Garcia Mestre <vgmestre@gmail.com>
2023-07-22 09:04:16 +0200

    change back post title

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -119,9 +119,9 @@ declare module 'astro:content' {
   collection: "blog",
   data: InferEntrySchema<"blog">
 } & { render(): Render[".mdx"] },
-"powershell-custom-prompt-setup.md": {
-  id: "powershell-custom-prompt-setup.md",
-  slug: "powershell-custom-prompt-setup",
+"powershell-prompt-with-oh-my-posh.md": {
+  id: "powershell-prompt-with-oh-my-posh.md",
+  slug: "powershell-prompt-with-oh-my-posh",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
