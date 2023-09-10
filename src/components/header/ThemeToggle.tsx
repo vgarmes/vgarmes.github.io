3b314c9ae8dfa79e0bc5e03c8a049d5a12ef07b3
Victor Garcia Mestre <vgmestre@gmail.com>
2023-09-11 00:24:42 +0200

    change theme toggle

--- src/components/header/ThemeToggle.tsx
+++ src/components/header/ThemeToggle.tsx
@@ -1,52 +1,13 @@
 import type { FunctionalComponent } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
+import Toggle from './Toggle'
 
-const icons = {
-	sun: (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="feather feather-sun"
-		>
-			<circle cx="12" cy="12" r="5"></circle>
-			<line x1="12" y1="1" x2="12" y2="3"></line>
-			<line x1="12" y1="21" x2="12" y2="23"></line>
-			<line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-			<line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-			<line x1="1" y1="12" x2="3" y2="12"></line>
-			<line x1="21" y1="12" x2="23" y2="12"></line>
-			<line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-			<line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-		</svg>
-	),
-	moon: (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="feather feather-moon"
-		>
-			<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-		</svg>
-	)
-}
+type Theme = 'dark' | 'light'
+
 const ThemeToggle: FunctionalComponent = () => {
-	const [theme, setTheme] = useState(() => {
+	const [theme, setTheme] = useState<Theme | null>(() => {
 		if (typeof window === 'undefined') {
-			return
+			return null
 		}
 		return document.documentElement.classList.contains('dark')
 			? 'dark'
@@ -68,15 +29,7 @@ const ThemeToggle: FunctionalComponent = () => {
 		setTheme(newTheme)
 	}
 
-	return (
-		<button
-			className="animate-fadein flex items-center"
-			onClick={toggleTheme}
-			aria-label="Toggle theme"
-		>
-			{theme === 'light' ? icons.moon : icons.sun}
-		</button>
-	)
+	return <Toggle onClick={toggleTheme} theme={theme || 'dark'} />
 }
 
 export default ThemeToggle
