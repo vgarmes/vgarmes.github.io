e47112d3ebce1e81275097def31894f7845cbd86
Victor Garcia Mestre <vgmestre@gmail.com>
2025-04-05 14:17:04 +0200

    relative time card demo

---
+++ src/components/demos/RelativeTimeCardDemo.tsx
@@ -0,0 +1,11 @@
+import RelativeTimeCard from '../RelativeTimeCard'
+
+const RelativeTimeCardDemo = () => {
+	const date = new Date()
+	return (
+		<div className="flex justify-center">
+			<RelativeTimeCard date={date} />
+		</div>
+	)
+}
+export default RelativeTimeCardDemo
