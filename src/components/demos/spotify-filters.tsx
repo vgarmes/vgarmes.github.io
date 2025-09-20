4aba97f5036e30c2c043c91ec31a236b97394714
Victor Garcia Mestre <vgmestre@gmail.com>
2025-09-20 15:41:37 +0200

    feat: publish spotify filters

--- src/components/demos/spotify-filters.tsx
+++ src/components/demos/spotify-filters.tsx
@@ -6,7 +6,6 @@ type FilterType = (typeof FILTERS)[number]
 
 const SpotifyFilters: FunctionalComponent = () => {
 	const [activeFilter, setActiveFilter] = useState<null | FilterType>(null)
-	const [activeCategory, setActiveCategory] = useState<null | string>(null)
 
 	const handleFilterChange = (filter: FilterType) => {
 		const index = FILTERS.indexOf(filter)
@@ -21,18 +20,9 @@ const SpotifyFilters: FunctionalComponent = () => {
 
 			const distance =
 				filterRefs.current[index].offsetLeft - containerRef.current.offsetLeft
-			console.log(distance)
 
 			filterRefs.current[index].style.transform =
 				`translateX(calc(2.5rem - ${distance}px)`
-		}
-	}
-
-	const handleCategoryChange = (category: string) => {
-		if (activeCategory === category) {
-			setActiveCategory(null)
-		} else {
-			setActiveCategory(category)
 		}
 	}
 
@@ -76,7 +66,7 @@ const SpotifyFilters: FunctionalComponent = () => {
 					disabled={activeFilter !== null && activeFilter !== filter}
 					aria-checked={activeFilter === filter}
 					aria-hidden={activeFilter !== null && activeFilter !== filter}
-					className="flex h-8 cursor-pointer items-center justify-center rounded-2xl bg-neutral-800 px-4 text-sm text-white opacity-100 transition-colors duration-300 ease-in-out not-aria-checked:hover:brightness-125 disabled:pointer-events-none disabled:opacity-0 aria-checked:bg-green-600 aria-checked:text-black"
+					className="flex h-8 cursor-pointer items-center justify-center rounded-2xl bg-neutral-800 px-4 text-sm text-white opacity-100 transition-colors duration-300 ease-in-out not-aria-checked:hover:brightness-125 disabled:pointer-events-none disabled:opacity-0 aria-checked:bg-green-600 aria-checked:text-white dark:aria-checked:text-black"
 					style={{
 						transition: `transform 0.5s, background-color 0.1s, color 0.1s${activeFilter !== null && activeFilter !== filter ? '' : ', opacity 0.1s 0.2s'}`,
 						transform: 'translateX(-2.5rem)'
@@ -89,28 +79,6 @@ const SpotifyFilters: FunctionalComponent = () => {
 					{filter.charAt(0).toUpperCase() + filter.slice(1)}
 				</button>
 			))}
-
-			{activeFilter === 'playlists' &&
-				['By You', 'By Spotify', 'Downloaded'].map((playlist, index) => (
-					<button
-						key={playlist}
-						role="radio"
-						disabled={activeCategory !== null && activeCategory !== playlist}
-						aria-checked={activeCategory === playlist}
-						aria-hidden={activeCategory !== null && activeCategory !== playlist}
-						className="flex h-8 cursor-pointer items-center justify-center rounded-2xl bg-neutral-800 px-4 text-sm text-white opacity-100 transition-colors duration-300 ease-in-out not-aria-checked:hover:brightness-125 disabled:pointer-events-none disabled:opacity-0 aria-checked:bg-green-600 aria-checked:text-black"
-						style={{
-							transition: `transform 0.5s, background-color 0.1s, color 0.1s${activeCategory !== null && activeCategory !== playlist ? '' : ', opacity 0.1s 0.2s'}`,
-							transform: 'translateX(-2.5rem)'
-						}}
-						onClick={() => {
-							handleCategoryChange(playlist)
-						}}
-						ref={el => (filterRefs.current[index] = el)}
-					>
-						{playlist.charAt(0).toUpperCase() + playlist.slice(1)}
-					</button>
-				))}
 		</div>
 	)
 }
